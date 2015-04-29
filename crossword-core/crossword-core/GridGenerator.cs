cd0f68fe51e5f6d606bfc4df9146642bdb55c895
walliancez <frasse_96@hotmail.com>
2015-04-29 18:35:29 +0200

    Added solution

---
+++ crossword-core/crossword-core/GridGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crossword_core
+{
+    class GridGenerator
+    {
+        private int width;
+        private int height;
+        private int complexity;
+
+        public GridGenerator()
+        {
+            this.width = width;
+        }
+    }
+}
---
+++ crossword-core/crossword-core/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crossword_core
+{ 
+    class Program
+    {
+        public void Run()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+    }
+}
